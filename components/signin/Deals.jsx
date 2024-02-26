1c8de987663f3d3b124b90da929966868bc40aa7
GauravTheBeginner <gaurav8861r@gmail.com>
2024-02-26 06:47:08 +0530

    commit

--- components/signin/Deals.jsx
+++ components/signin/Deals.jsx
@@ -6,8 +6,8 @@ function Deals() {
             <div className="flex justify-between items-center max-w-[70rem] w-[100%]">
                <p className=" text-[30px] text-[#5C6874] w-96 font-[400]">Sign up and get exclusive special deals</p>
                 <div class="flex">
-                    <input type="text" class="px-4 py-2 border outline-none rounded-l" />
-                    <button class="px-4 py-2 bg-blue-500 text-[14px] text-white rounded-r">Sign Up</button>
+                    <input type="text" class="px-4 py-2 border outline-none rounded-l-[12px]" />
+                    <button class="px-4 py-2 bg-[#1B88F4] text-[14px] text-white  rounded-r-[12px]">Sign Up</button>
                 </div>
 
             </div>
